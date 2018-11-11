0617ae3fb9d800a35ca73dc43ecd8f21d30bd80a
Eric Lau <ericlauchiho@gmail.com>
2018-11-11 16:48:09 +0800

    minor fix

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,9 +1,11 @@
 import auth from "./auth";
 import StorageManager from "./storageManager";
 import creator from "./creator";
+import networkClient from "./networkClient";
 
 export default {
     auth,
     StorageManager,
     creator,
+    networkClient,
 };
