a3b3f7bacbcfb3a937fbafcef8c8118b1e7a99c8
Unknown <elven118@gmail.com>
2018-12-31 00:57:56 +0800

    Revert "add message into sidebar"
    This reverts commit 2d92718561d7eb780e8634071bc3d1472956696c.

--- src/screens/sidebar/index.js
+++ src/screens/sidebar/index.js
@@ -26,15 +26,7 @@ const datas = [
     route: "SettingPage",
     icon: "cog",
   },
-<<<<<<< Updated upstream
   
-=======
-  {
-    name: "Message",
-    route: "MessagePage",
-    icon: "message",
-  },
->>>>>>> Stashed changes
 ];
 
 class SideBar extends Component {
